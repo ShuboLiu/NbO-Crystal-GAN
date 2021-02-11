b86b4389857b41d127bb83752cc0b5eae532cbda
ShuboLiu <63893167+ShuboLiu@users.noreply.github.com>
2021-02-11 13:39:39 +0800

    delete unused data

--- 5.data_post_process.py
+++ 5.data_post_process.py
@@ -1,7 +1,9 @@
 import pandas as pd 
 import numpy as np 
 import xlrd
-import os
+import os, sys
+
+os.removedirs("/OUTPUT")
 
 raw_data = np.load(r"fake_imgs_gen.npy")
 print("We have raw data of shape",raw_data.shape)
@@ -10,6 +12,28 @@ print("We have raw data of shape",raw_data.shape)
 数据转换
 将晶格常数转换为晶格矢量
 """
+data = np.zeros((((len(raw_data), 9, 3))))
+for i in range(len(data)):
+    vec_a = np.zeros((3, 1))
+    vec_b = np.zeros((3, 1))
+    vec_c = np.zeros((3, 1))
+    a = raw_data[i, 0, 0]
+    b = raw_data[i, 0, 1]
+    c = raw_data[i, 0, 2]
+    alpha = raw_data[i, 1, 0]
+    beta = raw_data[i, 1, 1]
+    gamma = raw_data[i, 1, 2]
+    vec_a = [a, 0, 0]
+    vec_b = [b*np.cos(alpha), b*np.sin(alpha), 0]
+    vec_c = [c*np.cos(gamma), 
+             c*((np.cos(beta) - np.cos(alpha)*np.cos(gamma))/np.sin(alpha)), 
+             np.sqrt(c**2 - c**2*np.cos(gamma)**2) - c**2*((np.cos(beta)**2 + np.cos(alpha)**2*np.cos(gamma)**2*c**2 - 2*c**2*np.cos(alpha)*np.cos(beta)*np.cos(gamma))/np.sin(alpha)**2)]
+    data[i, 0, :] = vec_a[:]
+    data[i, 1, :] = vec_b[:]
+    data[i, 2, :] = vec_c[:]
+
+    for j in range(2, raw_data.shape[1]):
+        data[i, j+1, :] = raw_data[i, j, :]
 
 """
 此处应该加入数据修正部分代码
@@ -18,8 +42,8 @@ print("We have raw data of shape",raw_data.shape)
 """
 
 ## 数据转存为POSCAR文件
-print(raw_data.shape[0])
-for i in range(raw_data.shape[0]):
+print(data.shape)
+for i in range(data.shape[0]):
     out_dir = "NbO_" + str(i)
     cur_path = os.path.abspath(os.curdir)
     out_name=os.path.join(cur_path, "OUTPUT", out_dir)
@@ -27,10 +51,10 @@ for i in range(raw_data.shape[0]):
         f.write('Nb O \n') 
         f.write('1.0 \n') 
         for j in range(2, 4):
-            f.write(str(raw_data[i, j, :]) + ' \n') 
+            f.write(str(data[i, j, :]) + ' \n') 
         f.write('Nb O \n')
         f.write('3 3 \n')
         f.write('Direct \n')
         for j in range(2, 8):
-            if abs(sum(raw_data[i, j, :])) < 1000:
-                f.write(str(raw_data[i, j, :]) + ' \n')
+            if abs(sum(data[i, j, :])) < 1000:
+                f.write(str(data[i, j, :]) + ' \n')
