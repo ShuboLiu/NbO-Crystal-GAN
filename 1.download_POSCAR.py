a41fa9dbffce9f8fcd345b66e2db70f19a76a579
ShuboLiu <63893167+ShuboLiu@users.noreply.github.com>
2021-03-02 16:56:53 +0800

    update

--- 1.download_POSCAR.py
+++ 1.download_POSCAR.py
@@ -4,11 +4,13 @@ import wget
 import pymysql
 import pandas as pd
 import time
+import subprocess
 
 #从OQMD数据库导出NbO数据
 present_dir = os.path.abspath(os.curdir)
 file_path = os.path.join(present_dir, "NbO.csv")
-data = pd.read_csv(file_path,header=None)
+## 第一行id，第二行entry_id
+data = pd.read_csv(file_path, header=None)
 id = data.iloc[0,:]
 entry_id = data.iloc[1,:]
 print(entry_id.shape)
@@ -17,24 +19,29 @@ print(id.shape)
 #开始下载POSCAR文件
 for i in range(1, len(id)):
     id_i=id[i]
-    id_int=str(id_i)
+    id_int=str(int(id_i))
     url = 'http://oqmd.org/materials/export/primitive/poscar/'+id_int
     output_file_name = id_int
     present_dir=os.path.abspath(os.curdir)
-    out_path=os.path.join(present_dir, "POSCAR", str(entry_id[i]))
-    out_name=os.path.join(present_dir, "POSCAR", str(entry_id[i]), output_file_name)
+    out_path=os.path.join(present_dir, "POSCAR", str(int(entry_id[i])))
+    out_name=os.path.join(present_dir, "POSCAR", str(int(entry_id[i])), output_file_name)
     if not os.path.exists(out_path):
         os.makedirs(out_path) 
         print(out_path + ' 创建成功')    
     if not os.path.exists(out_name):
-        command='wget ' + url + ' -O ' + out_name + ' --tries=5 -o wget.log'
-        #os.system(command)
-        wget.download(url, out_name)
+        command='wget ' + url + ' -O ' + id_int #+ ' --tries=5 -o wget.log'
+        args = [r"C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe", command]
+        #os.system(command) ## 用于linux主机
+        print(out_path)
+        subprocess.run(args, shell=False, cwd=out_path)
+        time.sleep(1)
         print(out_name + ' 下载成功')
     size = os.path.getsize(out_name)
     if size == 0:
         os.system('rm '+out_name)
-        command='wget ' + url + ' -O ' + out_name + ' --tries=5 -o wget.log'
-        #os.system(command)
-        wget.download(url, out_name)
+        command='wget ' + url + ' -O ' + out_name #+ ' --tries=5 -o wget.log'
+        args = [r"C:\WINDOWS\system32\WindowsPowerShell\v1.0\powershell.exe", command]
+        subprocess.run(args, shell=False, cwd=out_path)
+        #os.system(command) ## 用于linux主机
+        #wget.download(url, out_name)
         print(out_name + ' 清除并下载成功')
